07cc5ab3cac093f2b6171f2a97c866492d8e30c5
Superstar64 <Hexagonalstar64@gmail.com>
2016-07-29 02:14:07 -0400

    added license to top of each file

--- source/nbt.d
+++ source/nbt.d
@@ -1,3 +1,17 @@
+/+Copyright (C) 2015  Freddy Angel Cubas "Superstar64"
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
++/
 module nbt;
 import tagged_union;
 import std.array;
