05e2bf08d395a0ddd63dc42e490716b6dbe2f0ca
Superstar64 <Hexagonalstar64@gmail.com>
2017-03-04 00:33:37 -0500

    update file's header copyright

--- source/level.d
+++ source/level.d
@@ -1,17 +1,20 @@
-/+Copyright (C) 2015  Freddy Angel Cubas "Superstar64"
+/+
+	This file is part of Superstar64's World Generator.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation,  version 3 of the License.
+	Superstar64's World Generator is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	Superstar64's World Generator is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with Superstar64's World Generator.  If not, see <http://www.gnu.org/licenses/>.
 +/
+module level;
 import nbt;
 import std.algorithm;
 import std.bitmanip;
