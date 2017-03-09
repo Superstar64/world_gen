53a0c96972b2fc5626409a06f92f083928ec593b
Superstar64 <Hexagonalstar64@gmail.com>
2017-03-08 20:51:37 -0500

    forgot to remove or later from copyright headers

--- source/level.d
+++ source/level.d
@@ -3,8 +3,7 @@
 
 	Superstar64's World Generator is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+	the Free Software Foundation version 3 of the License..
 
 	Superstar64's World Generator is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
